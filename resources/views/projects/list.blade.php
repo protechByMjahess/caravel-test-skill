48450776655e9f45d6b843e97ee1bf2967e510c1
lg miniFridge <touficmjahess29@gmail.com>
2025-09-21 11:38:52 +0300

    Add projects list page

---
+++ resources/views/projects/list.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Projects List')
+
+@section('content')
+    @livewire('projects-list')
+@endsection
+
+@push('styles')
+<style>
+    .line-clamp-2 {
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
+</style>
+@endpush
+
+@push('scripts')
+<!-- Font Awesome for icons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+@endpush
